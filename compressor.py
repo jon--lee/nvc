ddb77ed8da338e434727ec588f20dd4ba402373f
Jonathan Lee <123abcjonathanlee@gmail.com>
2015-08-06 00:29:41 -0400

    update before modularize

--- compressor.py
+++ compressor.py
@@ -1,14 +1,16 @@
-#c
-
 from PIL import Image
 import neuralpy2
-import vectorizer
 import numpy as np
 
 
 
 
 class CompressorBase:
+
+    # initializer should take in a network that is used
+    # to compress and expand the data
+    def __init__(self, *args):
+        raise NotImplementedError
 
     # compress the image by propagating halfway through
     # the network (to the bottleneck) and returning the vector
@@ -21,39 +23,85 @@ class CompressorBase:
     def expand(self, *args):
         raise NotImplementedError
 
+    # propagate the image all the way forward.
+    # this function is primarily used for debuggin
+    # a trained replicator
     def forward(self, write_im):
         raise NotImplementedError
 
-    def train(self, read_im):
+    # train the network to recognize a set of images
+    # images must be vectorized and concatenated in order
+    # to properly train the network It is assumed that the
+    # network input size divides evenly into each image
+    def train(self, read_ims):
         raise NotImplementedError
 
+    # vectorize an matrix by collapsing
+    # the matrix into a single vector
     def _vectorize(self, *args):
         raise NotImplementedError
 
-    def _pix2vec(self, *args):
-        
+    # convert an PIL image object to an expanded vector
+    def _im2vec(self, *args):
+        raise NotImplementedError
+
+    # convert an expanded vector to a PIL image object
+    def _vec2im(self, *args):
+        raise NotImplementedError
 
 
 
 
 
 
-    def _vectorize(arr):
+class Compressor:
+
+    def __init__(self, net):
+        self.net = net
+
+
+    def train(im, epochs, learning_rate):
+        vec = self._im2vec(im)
+        max_ = self.net.start.size
+
+        # split vector that represents the entire image into short divisions
+        # that are easier to train. Together all the divisions in the list
+        # represent the image
+        training_inputs = [ vec[k*max_: (k+1)*max_] / 255 
+            for k in xrange(0, len(vec) / max_) ]
+        training_set = [(tvec, tvec) for tvec in training_inputs]
+
+        net.train(training_set, epochs, learning_rate, mini_batch_size=5, monitor=True)
+        return net    
+
+
+    def forward(self, im):
+        output_vec = np.array([])
+        for x, y in training_set:
+            out = (net.forward(x).reshape((len(x))) * 255).astype(int)
+            output_vec = np.concatenate((output_vec, out))
+
+        im = self._vec2im(im, output_vec)
+        return im
+
+
+    def _vectorize(self, arr):
         h,w = arr.shape
         num = h*w
         return np.reshape(arr, (num))
 
-    def _pix2vec(im):
+
+    def _im2vec(self, im):
         pix = im.load()
         w,h = im.size
         arr = np.zeros((h,w))
         for x in xrange(0, w):
             for y in xrange(0,h):
                 arr[y][x] = pix[y,x]
-        return _vectorize(arr)
+        return self._vectorize(arr)
 
 
-    def _vec2im(im, vec):
+    def _vec2im(self, im, vec):
         w,h = im.size
         arr = np.reshape(vec, (h,w))
         for x in xrange(0, w):
@@ -61,41 +109,3 @@ class CompressorBase:
                 im.putpixel((y, x), arr[y][x])
         return im
 
-
-
-
-
-def train(net, im):
-    vec = vectorizer.pix2vec(im)
-    max_ = net.start.size
-
-    # split vector that represents the entire image into short divisions
-    # that are easier to train. Together all the divisions in the list
-    # represent the image
-    training_inputs = [ vec[k*max_: (k+1)*max_] / 255 for k in xrange(0, len(vec) / max_) ]
-    training_set = [(tvec, tvec) for tvec in training_inputs]
-
-    net.train(training_set, 80, .3, mini_batch_size=5, monitor=True)
-    # net.show_cost()
-
-    return net    
-
-
-def prop_image(net, im):
-    output_vec = np.array([])
-    for x, y in training_set:
-        out = (net.forward(x).reshape((len(x))) * 255).astype(int)
-        output_vec = np.concatenate((output_vec, out))
-
-    im = vectorizer.vec2im(im, output_vec)
-    return im
-
-# assume this is black and white so pixels are not
-# tuples of rgb values
-im = Image.open('lena.bmp')
-
-np.set_printoptions(threshold=np.nan)
-net = neuralpy2.Network([64, 50, 64])
-im = train(net, im)
-im.show()
-im.save('replicated_3.bmp')
