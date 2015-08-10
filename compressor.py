0494329baa23cf52b952531a6a08266a5aee3580
Jonathan Lee <123abcjonathanlee@gmail.com>
2015-08-09 23:13:57 -0400

    add comments

--- compressor.py
+++ compressor.py
@@ -1,6 +1,16 @@
+#
+#   @author jonathan_lee@berkeley.edu (Jonatahn N. Lee)
+#
+#   This is a compression engine designed to compress image using neural
+#   a neural network from the neuralpy2 packagae found on PyPI. The compression
+#   is lossy and the network must be trained and then saved and reused.
+#   information is compressed via dimensionality reduction as the pixel
+#   inputs propagate through the network. 
+
 from PIL import Image
 import neuralpy2
 import numpy as np
+import sys
 
 
 
@@ -17,7 +27,6 @@ class CompressorBase:
     # at the halfway layer
     def compress(self, *args):
         raise NotImplementedError
-
     # expand the image by propagating from the middle layer
     # to the output layer which should reconstruct the image
     def expand(self, *args):
@@ -33,7 +42,13 @@ class CompressorBase:
     # images must be vectorized and concatenated in order
     # to properly train the network It is assumed that the
     # network input size divides evenly into each image
-    def train(self, read_ims):
+    def train(self, im, *args):
+        raise NotImplementedError
+
+    # compute the training set by breaking the image down
+    # into a single vector and subsequently breaking the vector
+    # down into input vectors that match the input for the neural network
+    def _compute_set(im):
         raise NotImplementedError
 
     # vectorize an matrix by collapsing
@@ -61,13 +76,13 @@ class Compressor:
 
 
     def train(self, im, epochs, learning_rate):
-        training_set = self._compute_training_set(im)
+        training_set = self._compute_set(im)
         self.net.train(training_set, epochs, learning_rate, mini_batch_size=5, monitor=True)
         return self.net    
 
 
     def forward(self, im):
-        training_set = self._compute_training_set(im)
+        training_set = self._compute_set(im)
         output_vec = np.array([])
         for x, y in training_set:
             out = (self.net.forward(x).reshape((len(x))) * 255).astype(int)
@@ -76,8 +91,9 @@ class Compressor:
         im = self._vec2im(im, output_vec)
         return im
 
-
-    def _compute_training_set(self, im):
+    # given an image, compute the training set which is a set of long vectors
+    # each of length that is equal to the size of the input vector of the network
+    def _compute_set(self, im):
         vec = self._im2vec(im)
         max_ = self.net.start.size
 
--- main.py
+++ main.py
@@ -8,12 +8,15 @@ from PIL import Image
 
 # assume this is black and white so pixels are not
 # tuples of rgb values
-im = Image.open('peppers.bmp')
+im = Image.open('images/training/lena.bmp')
 net = neuralpy2.Network([64, 50, 64])
 com = compressor.Compressor(net)
 
-com.train(im, 10, .3)
-im = com.forward(im)
+com.train(im, 20, .3)
 
-im.show()
-im.save('replicated_3.bmp')
+im2 = Image.open('images/testing/flower.bmp')
+im2 = im2.convert('RGB').convert('P', palette=Image.ADAPTIVE)
+im2 = com.forward(im2)
+
+im2.show()
+im2.save('images/products/lena_flower.bmp')
